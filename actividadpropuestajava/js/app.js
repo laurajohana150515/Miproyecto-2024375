e8213642b948fa7cd876a55ab93c4af59c622d4e
laurajohana150515 <ljrizo5@misena.edu.co>
2021-02-25 19:12:11 -0500

    first commit

---
+++ actividadpropuestajava/js/app.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18){
+    greeting = 'Buenas Noches!';
+} else if (hourNow > 12){
+    greeting = 'Buenas Tardes!';
+}else if (hourNow > 0){
+    greeting = 'Buenas Dias!';
+}else {
+    greeting = 'Bienvenidos!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
